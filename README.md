140ba608bfb438c3a9a07b445fe9fe58c7044513
mverch67 <manuel.verch@gmx.de>
2024-02-24 22:20:31 +0100

    initial project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Meshtastic device-ui project
+
+<!--Project specific badges here-->
+
+
+[![CLA assistant](https://cla-assistant.io/readme/badge/meshtastic/device-ui)](https://cla-assistant.io/meshtastic/device-ui-project)
+[![Fiscal Contributors](https://opencollective.com/meshtastic/tiers/badge.svg?label=Fiscal%20Contributors&color=deeppink)](https://opencollective.com/meshtastic)
+[![Vercel](https://img.shields.io/static/v1?label=Powered%20by&message=Vercel&style=flat&logo=vercel&color=000000)](https://vercel.com?utm_source=meshtastic&utm_campaign=oss)
+
+## Overview
+
+[Meshtastic](https://meshtastic.org) device-ui project for TFT screens
+
+## Stats
+
+![Alt](https://repobeats.axiom.co/api/embed/1205deb825cb7c902197ebab10aac3590ad8cc35.svg "Repobeats analytics image")
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include "DeviceScreen.h"
+#include "Arduino.h"
+
+DeviceScreen* screen = nullptr;
+
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+
+#ifdef KB_POWERON
+    digitalWrite(KB_POWERON, HIGH);
+    pinMode(KB_POWERON, OUTPUT);
+    delay(200); // wait until keyboard mcu startup finished
+#endif
+
+  Serial.println("\n*** SquareLine Studio (LovyanGFX TFT) ***");
+#ifdef ARDUINO_ARCH_ESP32
+  uint64_t chipid;
+  chipid = ESP.getEfuseMac(); //The chip ID is essentially its MAC address(length: 6 bytes).
+  Serial.printf("  ESP32 Chip ID = %04X", (uint16_t)(chipid >> 32)); //print High 2 bytes
+  Serial.printf("%08X\n\r", (uint32_t)chipid); //print Low 4bytes.
+  Serial.printf("  Flash size: %8d bytes\n\r", ESP.getFlashChipSize());
+  Serial.printf("  Heap size : %8d bytes\n\r", ESP.getHeapSize());
+  Serial.printf("  Free heap : %8d bytes\n\r", ESP.getFreeHeap());
+  Serial.printf("  PSRAM     : %8d bytes\n\r", ESP.getFreePsram());
+  Serial.println("*****************************************\n");
+#endif
+
+  screen = &DeviceScreen::create();
+  screen->init();
+
+#ifdef ARDUINO_ARCH_ESP32
+  Serial.printf("Free heap : %8d bytes\n\r", ESP.getFreeHeap());
+  Serial.printf("PSRAM     : %8d bytes\n\r", ESP.getFreePsram());
+#endif
+
+  Serial.println("Setup done.");
+}
+
+
+/*** main loop ***/
+void loop() {
+    screen->task_handler();
+    delay(20);
+}
