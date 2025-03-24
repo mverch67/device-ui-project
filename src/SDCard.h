1fe372983651dd7e38cee3933e050f7fed0936e8
mverch67 <manuel.verch@gmx.de>
2025-03-24 09:17:24 +0100

    fix compiler error when HAS_SDCARD is not defined

--- src/SDCard.h
+++ src/SDCard.h
@@ -3,12 +3,7 @@
 #include "Arduino.h"
 
 #ifndef ARCH_PORTDUINO
-// #include "SD.h"
 extern void setupSDCard();
 #else
 void setupSDCard() {}
-#endif
-
-#ifndef HAS_SDCARD
-#define HAS_SDCARD 0
 #endif
